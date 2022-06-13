a877ba97ee0a2cc101aacef6c926f60c24f22673
Babasky <boureimamaiga67@gmail.com>
2022-06-13 11:39:36 +0000

    devMaiga commit2

---
+++ créerreunion.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>créer reunion</title>
+</head>
+<body>
+    <img class="logo" src="/assets/image/logo.png" alt="">
+    <h1>créer une reunion</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <img class="img" src="assets/image/reunion 1.jpg" alt="">
+                <p>Démarer une reunion</p>
+            </div>
+            <div class="col-6">
+                <img class="img" src="assets/image/AJOUTER ME.jpg" alt="">
+                <p>Rejoindre une reunion</p>
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-6">
+                <img class="img" src="assets/image/calendrier.jpg" alt="">
+                <p>Programmer une reunion</p> 
+            </div>
+            <div class="col-6">
+                <img class="img" src="assets/image/icone_deconnex.jpg" alt="">
+                <p>Quitter reunion</p> 
+            </div> 
+        </div>        
+    </div>
+    
+    
+</body>
+</html>
